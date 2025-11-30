f480e39b5e1c9a3e226398d776d18b1d74eb5d42
KamrynSmithers <kamrynaliecesmithers@gmail.com>
2025-11-30 12:26:52 -0500

    added event listener to show cuurent device IP address

--- java.js
+++ java.js
@@ -1,1 +1,23 @@
-src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC_MH_FuNqanupJHTj_hs8o6J0f-a4xhKk"
+
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    mapId: "6875d1010bc8c5041e443859",
+    center: { lat: 33.7620, lng: -84.4172 },
+    zoom: 12
+  });
+}
+window.initMap = initMap;
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("https://api.ipify.org?format=json")
+    .then(response => response.json())
+    .then (data => {
+        document.getElementById("show").textContent = `Current Device IP Address: ${data.ip}`
+    })
+    .catch (error => {
+        console.error("Error Fetching IP", error)
+        document.getElementById("show").textContent = "Unable to retrieve IP address."
+    })
+
+})
